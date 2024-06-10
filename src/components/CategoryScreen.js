55222056d5d207958648d0dc5759c14b351e9dea
lashkevi4 <lashkevi4@users.noreply.github.com>
2024-06-10 16:45:50 +0300

    update page Category - work version

--- src/components/CategoryScreen.js
+++ src/components/CategoryScreen.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import BackButton from './BackButton';
 
 const categories = [
   { id: 1, name: 'Дети', icon: 'children.svg' },
@@ -16,8 +15,10 @@ function CategoryScreen() {
   return (
     <div style={styles.container}>
       <div style={styles.header}>
-        <BackButton />
-        <h1 style={styles.title}>Катагеории</h1>
+        <Link to="/" style={styles.backButton}>
+          <img src="/images/app/back.svg" alt="Назад" style={styles.backIcon} />
+        </Link>
+        <h1 style={styles.title}>Категории</h1>
       </div>
       <div style={styles.grid}>
         {categories.map(category => (
@@ -35,43 +36,59 @@ const styles = {
   container: {
     textAlign: 'center',
     padding: '20px',
+    backgroundColor: '#fff', // белый фон для всего контейнера
   },
   header: {
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center',
-    gap: '10px',
     marginBottom: '20px',
+    width: '100%',
+    maxWidth: '300px',
+    margin: '0 auto',
+  },
+  backButton: {
+    flex: '0 0 25%',
+    textAlign: 'left',
+  },
+  backIcon: {
+    width: '50px', // такая же ширина, как и у других иконок
+    height: '50px', // такая же высота, как и у других иконок
   },
   title: {
-    margin: 0,
+    flex: '0 0 75%',
+    fontSize: '36px', // тот же размер шрифта, что и на стартовой странице
+    fontFamily: 'Georgia, serif', // тот же шрифт, что и на стартовой странице
+    textAlign: 'center',
   },
   grid: {
     display: 'flex',
     flexDirection: 'column',
     gap: '10px',
-    alignItems: 'center', // центрируем элементы по горизонтали
+    alignItems: 'center',
   },
   item: {
     display: 'flex',
     alignItems: 'center',
     padding: '10px',
-    border: '1px solid #000',
+    border: '2px solid rgb(101, 98, 94)',
     borderRadius: '5px',
     textDecoration: 'none',
     color: '#000',
-    width: '100%',
+    width: '80%',
     maxWidth: '300px',
-    justifyContent: 'center', // центрируем элементы по горизонтали внутри
+    backgroundColor: 'rgb(247, 243, 238)',
+    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
   },
   icon: {
     width: '50px',
     height: '50px',
-    marginRight: '10px', // небольшое расстояние между картинкой и текстом
+    marginRight: '10px',
   },
   text: {
-    fontSize: '30px', // используем тот же размер шрифта, что и на главной странице
-    fontFamily: 'Georgia, serif', // используем тот же шрифт, что и на главной странице
+    fontSize: '30px',
+    fontFamily: 'Georgia, serif',
+    flex: 1,
+    textAlign: 'center',
   },
 };
 
