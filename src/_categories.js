9e99779b56e30420d2beebeb121372259e31018c
lashkevi4 <lashkevi4@users.noreply.github.com>
2024-07-01 22:12:32 +0300

    updatetes

---
+++ src/_categories.js
@@ -0,0 +1,9 @@
+export const categories = [
+  { id: 1, name: 'Дети', icon: 'children.svg', path: 'children' },
+  { id: 2, name: 'Женщины', icon: 'women.svg', path: 'women' },
+  { id: 3, name: 'Мужчины', icon: 'men.svg', path: 'men' },
+  { id: 4, name: 'Пары', icon: 'couples.svg', path: 'couples' },
+  { id: 5, name: 'Группы', icon: 'groups.svg', path: 'groups' },
+  { id: 6, name: 'Свадебные', icon: 'wedding.svg', path: 'wedding' },
+  { id: 7, name: 'Гламур', icon: 'glamour.svg', path: 'glamour' },
+];
